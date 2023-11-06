af19c8ba6e74d4436410cc3a7c214b57020c2299
Charles Korthout <charles.korthout@gmail.com>
2023-11-06 16:58:08 +0100

    1-3

---
+++ Day002/main.py
@@ -0,0 +1,15 @@
+#Create atip calculator that will add the tip (suggested 10, 12 or 15%)
+#It calculates the total amount including tip and will divide the result by the number of persons
+#The result will be rounded to two decimal places.
+print("Welcome to the tip calculator!")
+bill = float(input("What was the total bill? $"))
+tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
+people = int(input("How many people to split the bill?"))
+
+tip_as_percent = tip / 100
+total_tip_amount = bill * tip_as_percent
+total_bill = bill + total_tip_amount
+bill_per_person = total_bill / people
+final_amount = round(bill_per_person, 2)
+
+print(f"Each person should pay: ${final_amount}")
