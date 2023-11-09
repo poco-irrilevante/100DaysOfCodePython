399ee41177d7688cd042375592815d1663b63000
Charles Korthout <charles.korthout@gmail.com>
2023-11-09 14:49:24 +0100

    1-3

--- Day001/main.py
+++
@@ -1,8 +0,0 @@
-# Band Name Generator
-'''
-Create a Band name generator that concatenates the city where you grew up with your pet's name
-'''
-print("Welcome to the Band Name Generator.")
-street = input("What's the name of the city you grew up in?\n")
-pet = input("What's your pet's name?\n")
-print("Your band name could be " + street + " " + pet)
